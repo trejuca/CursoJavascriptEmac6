d5014a4025e5de005251ed054b3587e381ea5f0f
trejuca <110365594+trejuca@users.noreply.github.com>
2024-04-11 09:02:17 -0600

    Add files via upload

---
+++ Ejercicios/Proyecto/index.js
@@ -0,0 +1,85 @@
+
+// Constante para el tipo de api a aconsultar
+const API = "users"
+// URL de la api a consultare
+const URL = `https://jsonplaceholder.typicode.com/${API}`
+
+/**
+
+ */
+const consultarUsuario = async (id) => {
+	
+	try {		
+		const usuario = await fetch(`${URL}/${id}`)
+		
+		if (!usuario.ok) {
+			throw new Error(`No se encontro el usuario con el id: ${id}`)
+		}
+		const usuarioJson = await usuario.json()
+		cargarUsuario(usuarioJson)
+		
+	} catch (error) {
+		alert(error)
+	}
+}
+
+const leerUsuarioId = () => {
+	const usuarioId = document.querySelector("#busqueda").value
+	consultarUsuario(usuarioId)
+}
+
+const cargarUsuario = (usuario) => {
+	Object.keys(usuario).forEach(llave => {
+		let nodo = document.querySelector(`#${llave}`)
+		
+		if (nodo !== null) {
+			nodo.value = usuario[llave]
+		}
+	})
+}
+
+const guardarUsuario = () => {
+	
+	const usuario = construirUsuario()
+		
+	localStorage.setItem(usuario.id, JSON.stringify(usuario))
+	tablaComponente.actualizarTabla(usuario)
+}
+
+const inicializarAplicacion = () => {
+	
+	construirUsuario()
+	
+	tablaComponente.construirTabla("tabla-resultados", nombresColumnas)
+	
+	// Verificar si hay registros existentes en el localstorage
+//	for (let index = 0; index < localStorage.length; index++) {
+//		const usuario = JSON.parse(localStorage.getItem(localStorage.key(index)))
+//		tablaComponente.actualizarTabla(usuario)
+//	}
+	Object.keys(localStorage).forEach((valor) => {
+		const usuario = JSON.parse(localStorage.getItem(valor))
+		tablaComponente.actualizarTabla(usuario)
+	})
+}
+
+const construirUsuario = () => {
+	
+	const usuario = {}
+	const nombresColumnas = {}
+	
+	document.querySelectorAll(".user-form").forEach(nodo => {
+		usuario[nodo.id] = nodo.value
+		//nombresColumnas[nodo.value] = nodo.dataset.nombreColumna
+		//nombresColumnas[nodo.value] = nodo.dataset['nombreColumna']
+		nombresColumnas[nodo.id] = nodo.getAttribute("data-nombre-columna")
+	})
+	
+	return usuario
+}
+
+
+
+
+
+
---
+++ Ejercicios/Proyecto/tabla.js
@@ -0,0 +1,74 @@
+
+
+const tablaComponente = {
+	tabla: null,
+	columnas: {},
+	construirTabla: function(id, nombresColumnas) {
+		
+		let tablaTemp = document.getElementById(id)
+
+		if (!tablaTemp) {
+			this.tabla = document.createElement("table")
+			this.tabla.id = id
+			this.columnas = nombresColumnas
+			
+			const encabezado = this.tabla.createTHead().insertRow()
+			
+			Object.keys(nombresColumnas).forEach(function(key, index ) {
+				encabezado.insertCell(index).innerHTML = `<b>${nombresColumnas[key]}</b>`
+			})
+			
+			document.getElementById("div-resultados").appendChild(this.tabla)
+		}
+	},
+	construirFila: function() {
+		
+		if (Object.keys(this.columnas).length === 0) {
+			console.log("El objeto columnas esta vacio")
+			return {};
+		}
+		
+		const fila = this.tabla.insertRow()
+		const celdas = {}
+		
+		Object.keys(this.columnas).forEach(function(elemento, index) {
+			celdas[elemento] = fila.insertCell(index)
+		})
+		
+		return {fila, celdas}
+	},
+	actualizarTabla: function(datos) {
+
+		const datosTabla = this.construirFila()
+
+		Object.keys(datosTabla.celdas).forEach(function(elemento, index) {
+			datosTabla.celdas[elemento].innerHTML = datos[elemento]
+		})
+		
+		datosTabla.celdas['acciones'].appendChild(function() {
+			
+			const linkEliminar = document.createElement('a')
+			const linkActualizar = document.createElement('a')
+			
+			linkEliminar.innerText = "Eliminar"
+			linkEliminar.setAttribute("href", "")
+			linkEliminar.addEventListener("click", function(e) {
+				e.preventDefault()
+				localStorage.removeItem(datos.id)
+			})
+			
+			linkActualizar.innerText = "Actualizar"
+			linkActualizar.setAttribute("href", "")
+			linkActualizar.addEventListener("click", function(e) {
+				e.preventDefault()
+				cargarUsuario(datos)
+			})
+			
+			const div = document.createElement("div")
+			div.appendChild(linkEliminar)
+			div.appendChild(linkActualizar)
+			
+			return div
+		}())
+	}
+}
